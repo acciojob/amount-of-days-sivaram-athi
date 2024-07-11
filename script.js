09ea2473c5dccc3068bb7b5400f80bdd7c6c5328
sivaram-athi <137918320+sivaram-athi@users.noreply.github.com>
2024-07-11 15:49:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
 //your JS code here. If required.
 function daysOfAYear(year) {
+  // Check if the year is a leap year
+  if ((year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0)) {
+    return 366;
+  } else {
+    return 365;
+  }
+}
